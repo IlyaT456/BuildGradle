c0fee7bb8382904427348b8ba60633bc5cff35ff
Ks <IlyaT123@yandex.ru>
2024-12-15 22:14:57 +0300

    Gradle build project

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Результат сложение двух чисел = " + addition(4, 6));
+    }
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+}
